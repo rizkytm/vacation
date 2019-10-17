e04e980a39a21756a3031a0a63128a2f76e6c7e9
Rizky Tria Meditanala <ritrime97@gmail.com>
2019-10-17 21:49:31 +0700

    first commit

---
+++ src/app/places/places.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from "@angular/core";
+import { Place } from "./place.model";
+
+@Injectable({
+  providedIn: "root"
+})
+export class PlacesService {
+  private _places: Place[] = [
+    new Place(
+      "p1",
+      "Manhattan Mansion",
+      "In the heart of New York City",
+      "../assets/places-images/manhattan.jpg",
+      149.99
+    ),
+    new Place(
+      "p2",
+      "L'Amour Toujours",
+      "A romantic place in Paris!",
+      "../assets/places-images/eiffel.jpg",
+      189.99
+    ),
+    new Place(
+      "p3",
+      "The Foggy Palace",
+      "Not your average city trip!",
+      "../assets/places-images/foggy.jpg",
+      99.99
+    )
+  ];
+
+  get places() {
+    return [...this._places];
+  }
+
+  constructor() {}
+
+  getPlace(id: string) {
+    return { ...this._places.find(p => p.id === id) };
+  }
+}
